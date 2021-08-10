9e8e0d96ebca99ae8a7b2eb51d1c41733844e2a6
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-08-10 14:32:27 +0900

    [add]

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,18 @@
 class MapsController < ApplicationController
   def index
   end
+
+  def map
+    results = Geocoder.search(params[:address])
+    @latlng = results.first.coordinates
+
+    # respond_to以下の記述によって、
+    # remote: trueのアクセスに対して、
+    # map.js.erbが変えるようになります。
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :maps, only: [:index]
-
+  get '/map_request', to: 'maps#map', as: 'map_request'
 end
