4bd6c1adef8a996efe57fe28fa7b10f329b92726
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-08-15 12:03:42 +0900

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
+  get 'about' => 'homes#about', as: :about
 
   resources :maps, only: [:index]
   get '/map_request', to: 'maps#map', as: 'map_request'
