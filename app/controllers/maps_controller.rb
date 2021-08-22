2dc1c9b0c5c133e18a6d713a6b22da4ad4c2505b
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-08-22 23:57:48 +0900

    [add]

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -9,10 +9,10 @@ class MapsController < ApplicationController
     # respond_to以下の記述によって、
     # remote: trueのアクセスに対して、
     # map.js.erbが変えるようになります。
-    respond_to do |format|
-      format.js
-      format.json { render 'map', @latlng }
-    end
+    # respond_to do |format|
+    #   format.js
+    #   format.json { render 'map', @latlng }
+    # end
   end
 
 end
